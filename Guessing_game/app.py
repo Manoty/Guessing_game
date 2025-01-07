6165f77d6c7d0bdab9dfafb85b2bdf1a9bafbaee
manoty <kevinnoty21@gmail.com>
2025-01-07 21:04:26 +0300

    add input validation to guessing game

--- Guessing_game/app.py
+++ Guessing_game/app.py
@@ -3,10 +3,13 @@ guess_count = 0
 guess_limit = 3
 
 while guess_count < guess_limit:
-    guess = int(input('guess: '))
-    guess_count += 1
-    if guess == secret_number:
-        print('You won!')
-        break
+    try:
+        guess = int(input('guess: '))
+        guess_count += 1
+        if guess == secret_number:
+            print('You won!')
+            break
+    except ValueError:
+        print("please enter a valid no")
 else:
     print("sorry, you failed")    
